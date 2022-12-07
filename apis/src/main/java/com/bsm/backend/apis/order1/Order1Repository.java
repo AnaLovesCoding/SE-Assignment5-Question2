3168f729e08b9464abfca591e5687385dd688d1d
Archana G V <gvarchana95@gmail.com>
2022-12-06 20:05:32 -0800

    Question 2

--- apis/src/main/java/com/bsm/backend/apis/order1/Order1Repository.java
+++
@@ -1,7 +0,0 @@
-package com.bsm.backend.apis.order1;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Order1Repository extends JpaRepository<Order1, Long> {
-}
--- apis/src/main/java/com/bsm/backend/apis/orderdetail/Order_DetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bsm.backend.apis.orderdetail;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Order_DetailRepository extends JpaRepository<Order_Detail,Long> {
-}
